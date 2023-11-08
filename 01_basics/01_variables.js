6a8b9d001b9549cd1f91e5cda3c4f865e5265adb
Ajay Maurya <ajm44687@gmail.com>
2023-11-08 08:45:28 +0530

    conversion of data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 232434
+let accountEmail = "ajm44687@gmail.com"
+var accountPassword = "3232"
+accountCity = "Azamgarh"
+let accountState
+//accountId = 2//not allow
+accountEmail = "ajaydigito@gmail.com"
+accountPassword = "434"
+accountCity = "Jaunpur"
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) //we are using node js, not browser
+let name = "Ajay"
+let age = 27
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value (representation of empty value)
+//undefiend => 
+//symble => unique
+
+
+//object =>
+
+console.log(typeof undefined); //undefiend
+console.log(typeof null); // object
